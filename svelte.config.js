ca637d9a63a09f646efc62064d4186849e9257cb
Rebekka Øvensen Aanderaa <125483437+rebekk1110@users.noreply.github.com>
2025-03-06 11:19:04 +0100

    Updated base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,4 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/change_blindness_frontend/'  // Set this to match your repo name
 });
